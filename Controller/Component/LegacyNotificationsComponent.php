fd4d0fe57b198a3159e04316069194912ce27f7a
Cauan Cabral <cauan@radig.com.br>
2011-12-07 17:12:45 -0300

    Definindo URI como um array no NotificationsComponent
    Inclusão de um componente para permitir receber notificações da API v1

--- Controller/Component/LegacyNotificationsComponent.php
+++ Controller/Component/LegacyNotificationsComponent.php
@@ -1,6 +1,11 @@
 <?php
+App::uses('HttpSocket', 'Network/Http');
+App::uses('Xml', 'Utility');
 /**
  * Plugin de integração com a API do PagSeguro e CakePHP.
+ * 
+ * Componente para recuperar e verificar notificação de pagamento
+ * para a versão 1 da API do PagSeguro.
  *
  * PHP versions 5+
  * Copyright 2010-2011, Felipe Theodoro Gonçalves, (http://ftgoncalves.com.br)
@@ -14,17 +19,13 @@
  * @license      MIT License (http://www.opensource.org/licenses/mit-license.php)
  * @version      2.0
  */
-
-App::uses('HttpSocket', 'Network/Http');
-App::uses('Xml', 'Utility');
-
-class NotificationsComponent extends Component {
-
+class LegacyNotificationsComponent extends Component
+{
 	public $timeout = 20;
 
 	public $pgURI = array(
-		'host' => 'ws.pagseguro.uol.com.br',
-		'path' => '/v2/transactions/notifications/',
+		'host' => 'pagseguro.uol.com.br',
+		'path' => '/pagseguro-ws/checkout/NPI.jhtml',
 		'scheme' => 'https',
 		'port' => '443'
 	);
@@ -43,7 +44,7 @@ class NotificationsComponent extends Component {
 	 * 
 	 * @var string
 	 */
-	public $notificationCode = null;
+	public $transacaoID = null;
 	
 	/**
 	 * Construtor padrão
@@ -81,14 +82,8 @@ class NotificationsComponent extends Component {
 	 * @return bool Válido ou não
 	*/
 	public function isNotification($request) {
-		if(
-			$request->is('post') &&
-			isset($request->data['notificationCode']) &&
-			isset($request->data['notificationType']) &&
-			strlen($request->data['notificationCode']) == 39 &&
-			$request->data['notificationType'] == 'transaction'
-		) {
-			$this->notificationCode = $request->data['notificationCode'];
+		if($request->is('post') && isset($request->data['TransacaoID'])) {
+			$this->transactionID = $request->data['TransacaoID'];
 			
 			return true;
 		}
@@ -100,25 +95,58 @@ class NotificationsComponent extends Component {
 	 * Valida a notificação recebida e requisita da API do PagSeguro a situação de um pagamento,
 	 * converte o retorno de XML para Array e então o retorna.
 	 * 
-	 * @param string $code Código da notificação
+	 * @param CakeRequest $request Requisição
 	 * @return mixed array com dos dados da notificação em caso de sucesso, null em caso de falha
 	 */
-	public function getNotification($code = null) {
-		if(!empty($code) && is_string($code)) {
-			$this->notificationCode = $code;
-		}
+	public function getNotification($request) {
+		
+		$decoded = $this->decode($request->data);
 		
 		$HttpSocket = new HttpSocket(array('timeout' => $this->timeout));
 		
-		$this->pgURI['path'] .= '/' . $this->notificationCode;
+		$extraParams = array(
+			'Token' => $this->__config['token'],
+			'Comando' => 'Validar'
+		);
 
-		$response = $HttpSocket->get($this->pgURI, array('email' => $this->__config['email'], 'token' => $this->__config['token']));
+		$response = $HttpSocket->post($this->pgURI, array_merge($request->data, $extraParams));
 		
-		if(empty($response['body'])) {
-			return false;
+		if(strtolower($response['body']) != 'verificado') {
+			CakeLog::write('debug', "Notificação Legacy com código {$this->transacaoID} não pode ser verificada.");
 		}
 		
-		return Xml::toArray(Xml::build($response['body']));
+		return $decoded;
+	}
+	
+	/**
+	 *
+	 * @param array $data
+	 * @return array 
+	 */
+	protected function decode($data) {
+		$amount = 0;
+		
+		// Cálcula total da compra
+		for($i = 1; true; $i++)
+		{
+			if(isset($data['ProdValor_' . $i]))
+			{
+				$amount += ($data['ProdValor_' . $i] * $data['ProdQuantidade_' . $i]);
+			}
+		}
+		
+		$amount += $data['ValorFrete'];
+		
+		$decoded = array(
+			'transaction' => array(
+				'code' => $data['TransacaoID'],
+				'date' => $data['DataTransacao'],
+				'value' => $amount,
+				'status' => $data['StatusTransacao']
+			)
+		);
+		
+		return $decoded;
 	}
 
 	/**
